224f1ffe8c15325664c9924e151118a8109789c4
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 12:37:55 +0700

    splash screen

---
+++ NetflixSplashScreen/NetflixSplashScreen/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  NetflixSplashScreen
+//
+//  Created by Adrian Suryo Abiyoga on 11/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SplashScreen()
+            .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetflixSplashScreen/NetflixSplashScreen/NetflixSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixSplashScreenApp.swift
+//  NetflixSplashScreen
+//
+//  Created by Adrian Suryo Abiyoga on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NetflixSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
